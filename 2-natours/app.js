8a2b7451d6e51e71adff7ff9ce9f8f636334e39f
Aura Subhadeep <subhadeepadhikari5@gmail.com>
2023-07-04 12:46:46 +0530

    Update app.js

--- 2-natours/app.js
+++ 2-natours/app.js
@@ -3,7 +3,11 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.status(200).send('Hello from the server side!')
+    res.send('Hello from the server side!')
+})
+
+app.post('/', (req, res) => {
+    res.send('You can post now')
 })
 
 const port = 8000
