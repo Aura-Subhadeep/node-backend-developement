2ba9f2d4ee9b8b2f889ab8ff95abbcbf47d93baa
Aura Subhadeep <subhadeepadhikari5@gmail.com>
2023-07-04 15:55:18 +0530

    go

--- 2-natours/app.js
+++ 2-natours/app.js
@@ -1,17 +1,62 @@
+const fs = require('fs')
 const express = require('express')
 
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send('Hello from the server side!')
+app.use(express.json())
+
+// app.get('/', (req, res) => {
+//     res.send('Hello from the server side!')
+// })
+
+// app.post('/', (req, res) => {
+//     res.send('You can post now')
+// })
+
+const tours  = JSON.parse(
+    fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+)
+
+app.get('/api/v1/tours', (req, res) => {
+    res.json({
+        status: 'success',
+        results: tours.length,
+        data: {
+            tours
+        }
+    })
 })
 
-app.post('/', (req, res) => {
-    res.send('You can post now')
+app.get('/api/v1/tours/:id', (req, res) => {
+    const id = req.params.id * 1
+    const tour = tours.find(el => el.id === id)
+
+    // if (id > tours.length) {
+    if (!tour) {
+        return res.status(404).json({
+            status: 'failed'
+        })
+    }
+    res.json({
+        status: 'success',
+        tour
+    })
+})
+
+app.post('/api/v1/tours', (req, res) => {
+    const newId = tours[tours.length -1].id + 1
+    const newTour = Object.assign({ id: newId }, req.body)
+    tours.push(newTour)
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+        res.json({
+            data: {
+                tours: newTour
+            }
+        })
+    })
 })
 
 const port = 8000
-
 app.listen(port, () => {
     console.log(`The server is running on PORT ${port}`)
 })
